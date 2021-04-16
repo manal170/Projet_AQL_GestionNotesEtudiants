c66c7e3daf67bd28ff52ee3bdaca469cc3a1b12e
manal170 <2695754@gm>
2021-04-16 11:51:41 -0700

    Add a new class

---
+++ Examen3-AQL/Etudiant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen3_AQL
+{
+    class Etudiant
+    {
+        string nom;
+        string prenom;
+        int numeroEtudiant;
+    }
+}
