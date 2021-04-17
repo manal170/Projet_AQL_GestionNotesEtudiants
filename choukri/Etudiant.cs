48dd002b64e466319c62391dfb6923d504468d0a
choukri aiche <choukriaiche25@hotmail.fr>
2021-04-17 14:53:13 -0400

    la premiere classe etudiant

---
+++ choukri/Etudiant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Etudiant
+{
+	public int numero_etudaint = 0;
+	public String nom;
+    public String prenom;
+
+	public int Numro_etudiant { get; set; }
+	public String Nom { get; set; }
+	public String Prenom { get; set; }
+
+	public Etudiant(nom,prenom)
+	{
+		this.Nom = nom;
+		this.Prenom = prenom;
+		this.Numro_etudiant++;
+	}
+}
