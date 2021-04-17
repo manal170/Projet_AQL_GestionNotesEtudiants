f0b9c35f0b9eb61331cb1cd594e4cca6841c8d0b
manal170 <82121682+manal170@users.noreply.github.com>
2021-04-16 19:50:02 -0700

    Add files via upload

---
+++ Etudiant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen3_AQL
+{
+    class Etudiant
+    {
+        string nom;
+        string prenom;
+        int numeroEtudiant;
+
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public Etudiant(string nom ,string prenom)
+        {
+            this.Nom = nom ;
+            this.Prenom = prenom;
+        }
+    }
+}
