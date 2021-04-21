9ff16dc1a8b669b94743bbff8dc447903e115528
Rania-Ak <82183480+Rania-Ak@users.noreply.github.com>
2021-04-21 23:05:18 +0000

    Update Etudiant.cs

--- Etudiant.cs
+++ Etudiant.cs
@@ -4,23 +4,24 @@ public class Etudiant
 {
 	public Etudiant()
 	{
-		public int numero_etudaint = 10000;
+		private static int numero_etudaint = 10000;
 		public String nom;
 		public String prenom;
 
 		public String Nom { get; set; }
 		public String Prenom { get; set; }
+		public int Code { get; private set; }
 
-		public Etudiant(nom, prenom)
+		public Etudiant(String nom,String prenom)
 		{
 			this.Nom = nom;
 			this.Prenom = prenom;
-			this.Numro_etudiant++;
+			this.Code = ++numero_etudaint;
 		}
 		
 		public void Afficher()
 		{ 	
-		Console.WriteLine(" Le numero d'étudiant : " + this.Numero_etudiant + '\n' 
+		Console.WriteLine(" Le numero d'étudiant : " +this.Code + '\n' 
 				  + "Le nom de l'étudiant: " + this.Nom + '\n'
 				  + "Le prenom de l'étudiant : " + this.Prenom )
 		}
