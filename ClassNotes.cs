aed51918a68049920597971ecf5eb259aacebcd8
Zakariae Dahmane <2680333@collegelacite.ca>
2021-04-22 04:23:49 -0400

    correction de la class note

--- ClassNotes.cs
+++ ClassNotes.cs
@@ -2,15 +2,7 @@
 
 public class Notes
 {
-
-
-	public int numero_etudiant;
-	public int numero_cours;
-	public double UA1, UA2, UA3;
-
-	
-
-	public double Note { get; set; }
+    public double Note { get; set; }
 	public int NumeroEtudiant { get; set; }
 	public int NumeroCours { get; set; }
 
@@ -19,18 +11,10 @@ public class Notes
 		this.NumeroEtudiant = numero_etudiant;
 		this.NumeroCours = numero_cours;
 	}
-
-
+    
 	public double calculNotes()
 	{
 
 		return double note = UA1 * 0.35 + UA2 * 0.30 + UA3 * 0.35;
-	}
-
-	public void afficher()
-	{
-		Console.WriteLine(" Le numero d'étudiant est : " + this.NumeroEtudiant  + '\n'
-							+ "Numero du cours est : " + this.NumeroCours + '\n'
-							+ "Note de l'étudiant est : " + this.calculNotes());
 	}
 }
