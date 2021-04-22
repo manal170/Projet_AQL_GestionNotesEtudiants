5d3659ca0cb628f3e8e849667895330b9298fd31
allaouaboucheneb01 <2695953@collegelacite.ca>
2021-04-22 05:00:05 -0400

    Ajout de la class Cours dans le main

---
+++ Cours.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Cours
+{
+	public int numeroDeCours = 100;
+	public string titre;
+	public int code = 0;
+
+	public int NumeroDeCours { get; set; }
+	public string Titre { get; set; }
+	public int Code { get; set; }
+
+	public Cours(string titre, int numeroDeCours)
+    {
+		this.titre = titre;
+		numeroDeCours++;
+    }
+	public void Afficher()
+	{
+		Console.WriteLine("Le titre du cours: " + this.Titre + '\n' +
+				  "Le numero du cours: " + this.NumeroDeCours);
+	}
+}
