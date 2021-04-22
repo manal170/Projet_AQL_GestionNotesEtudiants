2f8913f353b9fd0961d8595aef5dce6b6ef79309
allaouaboucheneb01 <2695953@collegelacite.ca>
2021-04-22 04:58:58 -0400

    Ajout de la class Notes dans le main

---
+++ ClassNotes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Notes
+{
+
+
+
+	
+
+	public double Note { get; set; }
+	public int Numero_etudiant { get; set; }
+	public int Numero_cours { get; set; }
+
+	public Notes(int numero_etudiant, int numero_cours)
+	{
+		this.Numero_etudiant = numero_etudiant;
+		this.Numero_cours = numero_cours;
+	}
+
+
+	public double CalculNotes(double eA1, double eA2, double eA3)
+	{
+		double note = eA1 * 0.35 + eA2 * 0.30 + eA3 * 0.35;
+		return note;
+	}
+
+	
+}
