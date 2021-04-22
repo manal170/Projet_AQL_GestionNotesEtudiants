93265c5d8989694011d7814c21178a31c3564a0f
Mehdilatr <82611275+Mehdilatr@users.noreply.github.com>
2021-04-22 05:25:27 -0400

    Update Cours.cs

--- Cours.cs
+++ Cours.cs
@@ -2,22 +2,16 @@
 
 public class Cours
 {
-	public int numeroDeCours = 100;
-	public string titre;
-	public int code = 0;
-
+	
+	//Déclaration des attributs
 	public int NumeroDeCours { get; set; }
 	public string Titre { get; set; }
 	public int Code { get; set; }
-
+        //Déclaration des constructeurs 
 	public Cours(string titre, int numeroDeCours)
     {
-		this.titre = titre;
-		numeroDeCours++;
+		this.Titre = titre;
+		
     }
-	public void Afficher()
-	{
-		Console.WriteLine("Le titre du cours: " + this.Titre + '\n' +
-				  "Le numero du cours: " + this.NumeroDeCours);
-	}
+	
 }
